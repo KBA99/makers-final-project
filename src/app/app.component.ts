de7c8d79e884a58d57641b9630e72a013f9db29f
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-07-01 20:41:51 +0100

    formatting 💻: use tabs and 4 spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'makers-quiz-manager';
+	title = 'makers-quiz-manager';
 }
