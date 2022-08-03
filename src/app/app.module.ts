0602c1c8626b9c0c1a25682b20acc5cf1264dc89
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-03 10:07:14 +0100

    feat: add imports to app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './home-page/login/login.component';
+import { RegisterComponent } from './home-page/register/register.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+	declarations: [AppComponent, HomePageComponent, LoginComponent, RegisterComponent],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, SharedModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
