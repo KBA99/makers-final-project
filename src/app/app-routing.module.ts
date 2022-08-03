84f0442810ddc160a2765124667df217e2ab5d66
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-03 10:07:34 +0100

    feat: add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './home-page/login/login.component';
+import { RegisterComponent } from './home-page/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomePageComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
