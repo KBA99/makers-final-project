0811fbb6bf45ca077b41e23e00dc252fa54a3874
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-13 00:27:06 +0100

    feat: protect routes with AuthGuardService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { AuthGuardService } from './user-authentication/auth-guard.service';
 import { LoginComponent } from './user-authentication/login/login.component';
 import { RegisterComponent } from './user-authentication/register/register.component';
 
 const routes: Routes = [
-	{ path: '', component: HomePageComponent },
+	{ path: '', canActivate: [AuthGuardService], component: HomePageComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
-	{ path: 'not-found', component: PageNotFoundComponent },
+	{ path: 'not-found', canActivate: [AuthGuardService], component: PageNotFoundComponent },
 	{ path: '**', redirectTo: '/not-found'}
 ];
 
