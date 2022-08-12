c9f90ba123858a1b08e7020d2374e977ddf26cbe
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-12 22:04:43 +0100

    feat: add quiz rest controller

---
+++ src/server/controllers/quiz-rest-controller.js
@@ -0,0 +1,23 @@
+export const createQuiz = (req, res) => {
+
+};
+
+export const deleteQuiz = (req, res) => {
+
+};
+
+export const getAllQuiz = (req, res) => {
+
+};
+
+export const getQuizById = (req, res) => {
+
+};
+
+export const getQuizListByUser = (req, res) => {
+
+};
+
+export const updateQuizById = (req, res) => {
+
+};
