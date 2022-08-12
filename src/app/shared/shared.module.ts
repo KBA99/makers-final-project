c5c5f2d9a11d54199403724bea30da2dd278e944
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-12 22:16:28 +0100

    add default default quiz page and header component to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
+import { DefaultQuizPageComponent } from './default-quiz-page/default-quiz-page.component';
 
 
 @NgModule({
 	imports: [
         ReactiveFormsModule,
         MaterialModule,
-        CommonModule
+        CommonModule,
 	],
-    declarations: [],
+    declarations: [
+        DefaultQuizPageComponent,
+        HeaderComponent
+    ],
 	exports: [
         ReactiveFormsModule
     ],
