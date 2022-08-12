6d5fd144940e524448d898e7d73015058ec2f76f
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-12 22:28:45 +0100

    refactor: fix import file paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './home-page/login/login.component';
-import { RegisterComponent } from './home-page/register/register.component';
+import { LoginComponent } from './user-authentication/login/login.component';
+import { RegisterComponent } from './user-authentication/register/register.component';
 
 const routes: Routes = [
 	{ path: '', component: HomePageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './home-page/login/login.component';
-import { RegisterComponent } from './home-page/register/register.component';
+import { LoginComponent } from './user-authentication/login/login.component';
+import { RegisterComponent } from './user-authentication/register/register.component';
 import { SharedModule } from './shared/shared.module';
+import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
 
 @NgModule({
-	declarations: [AppComponent, HomePageComponent, LoginComponent, RegisterComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, SharedModule],
+	declarations: [
+		AppComponent,
+		HomePageComponent,
+		LoginComponent,
+		RegisterComponent,
+		UserAuthenticationComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		SharedModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
