3696e1b282b5298c49fe766216445b82162c9af1
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-12 22:07:38 +0100

    feaT: add question rest controller

---
+++ src/server/controllers/question-rest-controller.js
@@ -0,0 +1,19 @@
+export const createQuestion = (req, res, next) => {
+
+};
+
+export const deleteQuestion = (req, res, next) => {
+
+};
+
+export const getQuestionsByQuizId = (req, res, next) => {
+
+};
+
+export const getQuestionsByUser = (req, res, next) => {
+
+};
+
+export const updateQuestionById = (req, res, next) => {
+
+};
