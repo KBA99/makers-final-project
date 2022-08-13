62b5cc86f30e7888153a0819e06a259f7433aac7
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-13 22:30:02 +0100

    feat: add error message for incorrect password matching

--- src/app/user-authentication/register/register.component.ts
+++ src/app/user-authentication/register/register.component.ts
@@ -13,23 +13,33 @@ export class RegisterComponent extends UserAuthenticationComponent implements On
 	}
 
 	registerForm!: FormGroup;
-
+	passwordNoMatchError = false;
 
 	override ngOnInit(): void {
 		this.registerForm = new FormGroup({
 			firstName: new FormControl(null, [Validators.required, Validators.nullValidator]),
 			lastName: new FormControl(null, [Validators.required, Validators.nullValidator]),
-			email: new FormControl(null, [Validators.required, Validators.nullValidator]),
-			dateOfBirth: new FormControl(null, [Validators.required, Validators.nullValidator]),
+			email: new FormControl(null, [
+				Validators.required,
+				Validators.nullValidator,
+				Validators.email,
+			]),
 			password: new FormControl(null, [Validators.required, Validators.nullValidator]),
 			confirmPassword: new FormControl(null, [Validators.required, Validators.nullValidator]),
 		});
 	}
 
-
-
-
 	onRegister() {
+		if (this.registerForm.valid) {
+			if (this.registerForm.value.confirmPassword === this.registerForm.value.password) {
 
+			} else {
+				this.passwordNoMatchError = true;
+				setTimeout(() => {
+					this.passwordNoMatchError = false;
+				}, 5000);
+			}
+		}
+		// Logic to check if the email exists already
 	}
 }
