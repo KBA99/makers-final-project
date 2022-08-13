8eefa556852cd787f469a022a2078f89e2711452
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-13 09:21:11 +0100

    refactor: redirect to User authentication component instead of individual login and register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { AuthGuardService } from './user-authentication/auth-guard.service';
-import { LoginComponent } from './user-authentication/login/login.component';
-import { RegisterComponent } from './user-authentication/register/register.component';
+import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
 
 const routes: Routes = [
 	{ path: '', canActivate: [AuthGuardService], component: HomePageComponent },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
+	{ path: 'login', component: UserAuthenticationComponent },
 	{ path: 'not-found', canActivate: [AuthGuardService], component: PageNotFoundComponent },
 	{ path: '**', redirectTo: '/not-found'}
 ];
