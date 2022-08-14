bd735756fc79da7d04e7bbbc572f9a008c778140
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-14 10:51:30 +0100

    feat: add function to create a new user

--- src/server/services/user-service.js
+++ src/server/services/user-service.js
@@ -1,1 +1,25 @@
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
+
+
+export async function createNewUser(user) {
+    const { firstName, lastName, email, password }  = user;
+    const hashedAndSaltedPassword = await bcrypt.hash(password, 10);
+
+    const registerUserObject = await User.create({
+        firstName,
+        lastName,
+        email,
+        hashedAndSaltedPassword
+    });
+
+    return registerUserObject;
+}
+
+export const throwErrorIfUserIsNull = (user) => {
+    if(user === null) throw new Error("This user does not exist.")
+}
+
+export const throwErrorIfUserExists = (user) => {
+    if(user != null) throw new Error("This email has already been registered.")
+}
