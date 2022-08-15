10c9b5973e9a2640f323d155e121c6e571a56db6
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 11:41:08 +0100

    feat: add logout and menu button to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/user-authentication/auth.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+	constructor(private readonly authService: AuthService) {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	onLogout() {
+		this.authService.logout()
+	}
 }
