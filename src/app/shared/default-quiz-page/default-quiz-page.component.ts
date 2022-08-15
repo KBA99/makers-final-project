b4b7d59e81a300b6a96f948b78c61622e2853780
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 11:39:03 +0100

    feat: complete default page for the quiz manager

--- src/app/shared/default-quiz-page/default-quiz-page.component.ts
+++ src/app/shared/default-quiz-page/default-quiz-page.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-default-quiz-page',
-  templateUrl: './default-quiz-page.component.html',
-  styleUrls: ['./default-quiz-page.component.scss']
+	selector: 'app-default-quiz-page',
+	templateUrl: './default-quiz-page.component.html',
+	styleUrls: ['./default-quiz-page.component.scss'],
 })
 export class DefaultQuizPageComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
-
+	}
 }
