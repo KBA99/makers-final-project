1870c0a0213c08c621623c844153822b9a2e483a
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 13:49:14 +0100

    feat: add child route for quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { QuizComponent } from './home-page/quiz/quiz.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { AuthGuardService } from './user-authentication/auth-guard.service';
 import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
 
 const routes: Routes = [
-	{ path: '', canActivate: [AuthGuardService], component: HomePageComponent },
+	{ path: '', canActivate: [AuthGuardService], component: HomePageComponent, children: [
+		{path: 'quiz', component: QuizComponent}
+	]},
 	{ path: 'login', component: UserAuthenticationComponent },
 	{ path: 'not-found', canActivate: [AuthGuardService], component: PageNotFoundComponent },
-	{ path: '**', redirectTo: '/not-found'}
+	{ path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
