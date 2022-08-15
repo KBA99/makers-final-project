6bacffdfa0ccd564a6386a8801aee0af4ac589f9
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 04:40:45 +0100

    feat: complete authentication checking

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -2,18 +2,28 @@ import jwt from 'jsonwebtoken';
 import { config } from '../config.js';
 
 export function authenticateToken(req, res, next) {
-	const authHeader = req.headers['Authorization'];
+	const authHeader = req.headers['authorization'];
 	const token = authHeader?.split(' ')[1];
 
 	if (token == null) return res.sendStatus(401);
 
 	jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
 		console.log(err);
-
-		if (err) return res.sendStatus(403);
-
+		if (err) return res.sendStatus(403).send({error: err.message});
 		req.user = user;
 		next();
+	});
+}
+
+export function verifyToken(req, res) {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader?.split(' ')[1];
+
+	if (token == null) return res.sendStatus(401);
+
+	jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
+		console.log(err);
+		err ? res.sendStatus(401) : res.sendStatus(200)
 	});
 }
 
