4ce4fd038ad1e583c7120ea663a2457a9f36b919
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 04:45:01 +0100

    feat: navigate to home page if user is signed in

--- src/app/user-authentication/user-authentication.component.ts
+++ src/app/user-authentication/user-authentication.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-user-authentication',
-  templateUrl: './user-authentication.component.html',
-  styleUrls: ['./user-authentication.component.scss']
+	selector: 'app-user-authentication',
+	templateUrl: './user-authentication.component.html',
+	styleUrls: ['./user-authentication.component.scss'],
 })
 export class UserAuthenticationComponent implements OnInit {
+	constructor(protected readonly authService: AuthService, protected readonly router: Router) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.redirectToDashboardIfSignedIn();
+	}
 
-  ngOnInit(): void {
-  }
-
+	redirectToDashboardIfSignedIn() {
+		this.authService.validateToken();
+		if (this.authService.isAuthenticated()) {
+			this.router.navigate(['/']);
+		}
+	}
 }
