aab9228b028a05763169f2090fbca7a84c5a2c64
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 04:52:19 +0100

    refactor: reorganise methods

--- src/app/user-authentication/auth.service.ts
+++ src/app/user-authentication/auth.service.ts
@@ -13,27 +13,12 @@ export class AuthService {
 	protected authToken: string | null | undefined;
 	private _isLoggedIn: boolean | undefined
 
-	getToken() {
-		return localStorage.getItem('Authorization');
-	}
-
-	register(credentials: Credentials) {
-		this.http
-			.post(ApiPath.base + UserPath.register, credentials, { observe: 'response' })
-			.subscribe({
-				next: (data) => {
-					this.authToken = data.headers.get('Authorization');
-					this._isLoggedIn = (!!this.authToken);
-					this.setTokenInBrowser()
-				},
-				error: (error) => {
-					console.log(error);
-				},
-			});
-	}
-
 	isAuthenticated() {
 		return !!this.getToken()
+	}
+
+	getToken() {
+		return localStorage.getItem('Authorization');
 	}
 
 	login(credentials: Credentials) {
@@ -54,6 +39,21 @@ export class AuthService {
 	logout() {
 		this.authToken = null
 		localStorage.clear()
+	}
+
+	register(credentials: Credentials) {
+		this.http
+			.post(ApiPath.base + UserPath.register, credentials, { observe: 'response' })
+			.subscribe({
+				next: (data) => {
+					this.authToken = data.headers.get('Authorization');
+					this._isLoggedIn = (!!this.authToken);
+					this.setTokenInBrowser()
+				},
+				error: (error) => {
+					console.log(error);
+				},
+			});
 	}
 
 	setTokenInBrowser() {
