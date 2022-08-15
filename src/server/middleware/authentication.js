1157dadc86094468b0aee8b02979048e211a811a
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 01:20:31 +0100

    feat: implement generating jwt token

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -1,6 +1,26 @@
-import  jwt  from ('jsonwebtoken');
-import { config } from '../config.js'
+import jwt from 'jsonwebtoken';
+import { config } from '../config.js';
 
-export const authenticate = (req, res, next) => {
+function authenticateToken(req, res, next) {
+	const authHeader = req.headers['Authorization'];
+	const token = authHeader?.split(' ')[1];
 
+	if (token == null) return res.sendStatus(401);
+
+	jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
+		console.log(err);
+
+		if (err) return res.sendStatus(401);
+
+		req.user = user;
+		next();
+	});
+}
+
+export function generateAccessToken(user) {
+	const jwtObject = {
+		id: user._id,
+		email: user.email,
+	};
+	return jwt.sign(jwtObject, config.TOKEN_SECRET, { expiresIn: '10h' });
 }
