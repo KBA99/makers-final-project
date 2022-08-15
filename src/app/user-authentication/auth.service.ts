5687d60663a180db58cc74eccf73976165b0e67c
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 02:36:06 +0100

    feat: add login and register functions

--- src/app/user-authentication/auth.service.ts
+++ src/app/user-authentication/auth.service.ts
@@ -1,24 +1,62 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ApiPath, UserPath } from '../paths';
+import { Credentials } from './authentication.types';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthService {
+	constructor(private router: Router, 
+				private http: HttpClient) {}
+
+	protected authToken: string | undefined;
 	isLoggedIn = false;
 
-	constructor(
-		private router: Router
-	) {}
-
 	isAuthenticated() {
-        return this.isLoggedIn
+		return this.isLoggedIn;
 	}
 
-	login() {
+	login(credentials: Credentials) {
+		this.http.post(ApiPath.base + UserPath.login, credentials)
+		.subscribe({
+			next: (data) => {
+				console.log(data);
+				this.isLoggedIn = true;
+			},
+			error: (error) => {
+				console.log(error);
+			},
+		});
 	}
 
-	logout() {
+	logout() {}
+
+	register(credentials: Credentials) {
+		this.http.post(ApiPath.base + UserPath.register, credentials).subscribe({
+			next: (data) => {
+				console.log(data);
+				this.isLoggedIn = true;
+			},
+			error: (error) => {
+				console.log(error);
+			},
+		});
+	}
+
+	getToken() {
+		return localStorage.getItem('token');
+	}
+
+
+	async setBearerToken() {
+		// get token from fetch request
+		const res: any = '';
+		const token = await res.json();
+
+		// set token in cookie
+		document.cookie = `token=${token}`;
 	}
 }
+
