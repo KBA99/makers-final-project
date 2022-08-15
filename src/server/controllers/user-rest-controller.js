1634950a3eedc9fdb7bbb67667cab0172f6f400c
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 03:16:32 +0100

    feat: expose headers sent from backend

--- src/server/controllers/user-rest-controller.js
+++ src/server/controllers/user-rest-controller.js
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
         const registeredUserObject = await createNewUser(req.body)
     
         const jwtToken = generateAccessToken(registeredUserObject)
-        res.status(201).set('Authorization', `bearer ${jwtToken}`).send({data: "New user created."});
+        res.status(201).set('Authorization', `bearer ${jwtToken}`).set('Access-Control-Expose-Headers', '*').send({data: "New user created."});
 
         console.log(registeredUserObject)
 
@@ -30,7 +30,7 @@ export const login = async (req, res) => {
         authenticateUserByEmailAndPassword(password, user.password)
 
         const jwtToken = generateAccessToken(user)
-        res.status(200).set('Authorization', `bearer ${jwtToken}`).send({data: "User Authenticated."});
+        res.status(200).set('Authorization', `bearer ${jwtToken}`).set('Access-Control-Expose-Headers', '*').send({data: "User Authenticated."});
     } catch (error) {
         console.log(error.message);
         res.status(403).send({error: error.message}); // forbidden
