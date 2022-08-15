4294e732ad3bc32196286e53f99a6834d74033a1
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-08-15 04:49:23 +0100

    feat: add hashed password ot databse

--- src/server/services/user-service.js
+++ src/server/services/user-service.js
@@ -1,7 +1,6 @@
 import User from '../models/User.js'
 import bcrypt from 'bcrypt'
 
-
 export async function createNewUser(user) {
     const { firstName, lastName, email, password }  = user;
     const hashedAndSaltedPassword = await bcrypt.hash(password, 10);
@@ -10,7 +9,7 @@ export async function createNewUser(user) {
         firstName,
         lastName,
         email,
-        hashedAndSaltedPassword
+        password: hashedAndSaltedPassword
     });
 
     return registerUserObject;
@@ -20,10 +19,18 @@ export async function findUserByEmail(email) {
 	return await User.findOne({ 'email': email });
 }
 
+export async function authenticateUserByEmailAndPassword(plainPassword, hashedPassword) {
+	return await bcrypt.compare(plainPassword, hashedPassword)
+}
+
 export const throwErrorIfUserIsNull = (user) => {
     if(user === null) throw new Error("This user does not exist.")
 }
 
 export const throwErrorIfUserExists = (user) => {
     if(user != null) throw new Error("This email has already been registered.")
+}
+
+export const throwErrorIfPasswordIsIncorrect = (sucesssfullyLoggedIn) => {
+	if (!sucesssfullyLoggedIn) throw new Error("Incorrect credentials entered.")
 }
